fb3cdd1289f8a741121473d3223ac756cffb61a1
zeke-jeske <zeke.jeske@gmail.com>
2020-08-07 17:54:34 -0500

    Make it so the computer actually checks your guesses

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import Board from 'components/Board'
 import Button from 'react-bootstrap/Button'
 
 const codePegColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+const keyPegColors = ['red', 'white']
 
 interface State {
   rows: Array<{
@@ -12,6 +13,8 @@ interface State {
   code: string[]
   codeHidden: boolean
   activeRow: number
+  gameDone: boolean
+  playerWon: boolean
 }
 
 export default class App extends React.Component<{}, State> {
@@ -19,12 +22,14 @@ export default class App extends React.Component<{}, State> {
     rows: Array(10)
       .fill(null)
       .map(() => ({
-        guess: Array(4).fill(null),
-        response: Array(4).fill(null),
+        guess: Array(4).fill(''),
+        response: Array(4).fill(''),
       })),
-    code: ['red', 'yellow', 'orange', 'blue'],
+    code: generateCode(),
     codeHidden: true,
     activeRow: 9,
+    gameDone: false,
+    playerWon: false,
   }
 
   switchPegColor = (rowNum: number, pegNum: number) => {
@@ -43,6 +48,61 @@ export default class App extends React.Component<{}, State> {
       rows[rowNum] = row
 
       return { rows }
+    })
+  }
+
+  check = () => {
+    this.setState(state => {
+      const activeRow = state.activeRow
+      const newState: any = {}
+      const rows = state.rows
+      const guess = [...rows[activeRow].guess]
+      const code = [...state.code]
+      let response: string[] = []
+
+      guess.forEach((color, pegNum) => {
+        // correct color and correct position
+        if (color === code[pegNum]) {
+          // make sure it's not counted twice
+          guess[pegNum] = ''
+          code[pegNum] = ''
+          response.push(keyPegColors[0])
+        }
+      })
+
+      // if the player won
+      if (response.length === 4) {
+        newState.gameDone = true
+        newState.codeHidden = false
+        newState.playerWon = true
+      } else {
+        guess.forEach((color, pegNum) => {
+          // correct color, wrong place
+          if (color !== '' && code.includes(color)) {
+            code[code.indexOf(color)] = ''
+            response.push(keyPegColors[1])
+          }
+        })
+
+        response = response.concat(Array(4 - response.length).fill('')) // pad array to length 4
+
+        newState.activeRow = activeRow - 1
+
+        // player already made 10 guesses
+        if (!activeRow) {
+          newState.gameDone = true
+          newState.codeHidden = false
+        }
+      }
+
+      newState.rows = [...rows]
+
+      newState.rows[activeRow] = {
+        ...rows[activeRow],
+        response,
+      }
+
+      return newState
     })
   }
 
@@ -67,10 +127,18 @@ export default class App extends React.Component<{}, State> {
             bottom: 0,
             right: 0,
           }}
+          onClick={this.check}
+          disabled={this.state.rows[this.state.activeRow].guess.includes('')}
         >
           Check
         </Button>
       </div>
     )
   }
+}
+
+function generateCode(): string[] {
+  return Array(4)
+    .fill('')
+    .map(() => codePegColors[Math.floor(Math.random() * codePegColors.length)])
 }
