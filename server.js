faafe120719ca22f565f37938c95038eada868e8
zeke-jeske <56773338+zeke-jeske@users.noreply.github.com>
2020-08-10 11:41:55 -0500

    Redirect 404s to /index.html

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ const app = express()
 const buildPath = path.join(__dirname, 'build')
 const port = process.env.PORT || 3000
 
-console.log('Starting server.js...')
-
 app.use(express.static(buildPath))
 
 app.get('*', (req, res) => {
-  console.log('Received GET request!')
-  res.sendFile(path.join(buildPath, 'index.html'))
+  res.redirect('/')
 })
 
 app.listen(port, () => {
