72d1c1735541fa54a830d6baa8307739e0094a0e
zeke-jeske <56773338+zeke-jeske@users.noreply.github.com>
2020-08-11 14:37:37 -0500

    Simplify server.js and switch default port to 5000

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const buildPath = path.join(__dirname, 'build')
-const port = process.env.PORT || 3000
 
-app.use(express.static(buildPath))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', (req, res) => {
   res.redirect('/')
 })
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`)
-})
+app.listen(process.env.PORT || 5000)
